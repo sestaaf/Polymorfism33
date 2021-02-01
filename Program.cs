f0cdcc70d92d68a47de5b5e4994cffe410559565
Sven-Erik Staaf <sestaaf@live.se>
2021-02-01 15:56:28 +0100

    Skrev lite till...

--- Program.cs
+++ Program.cs
@@ -19,7 +19,9 @@ namespace Polymorfism33
 			}
 		}
 
-		// 3.4.11: Går att styra vad som ska gå att göra i ärvande klasser.
+		// 3.4.11: Går att styra vad som ska gå att göra i ärvande klasser
+		//         genom gemensamma gränssnitt i de översta arvsklasserna.
+		//         Subklasser kan ha egna subklasser som ärver från dem.
 		// 3.4.12: Blir mindre kod p g a att upprepningar kan undvikas,
 		//         vilket också gör att det blir lättare att följa flödet.
 		// 3.4.13: Metoder, egenskaper m m  som finns i ett interface måste 
